cad41924c4c711904bfbc53f4c6854be3d16b086
CharanRajG <105408255+CharanRajG@users.noreply.github.com>
2023-04-12 09:30:40 +0530

    Contract

--- OrderBook2.sol
+++ OrderBook2.sol
@@ -125,3 +125,106 @@ contract OrderBook2 is ReentrancyGuard, IOrderBook2 {
         require(msg.sender == fastPriceFeed, "OrderBook: forbidden");
         _;
     }
+    
+        constructor() public {
+        gov = msg.sender;
+    }
+
+    function initialize(
+        address _router,
+        address _vault,
+        address _weth,
+        address _usdf,
+        uint256 _minExecutionFee,
+        uint256 _minPurchaseTokenAmountUsd,
+        address _fastPriceFeed
+    ) external onlyGov {
+        require(!isInitialized, "OrderBook: already initialized");
+        isInitialized = true;
+
+        router = _router;
+        vault = _vault;
+        weth = _weth;
+        usdf = _usdf;
+        minExecutionFee = _minExecutionFee;
+        minPurchaseTokenAmountUsd = _minPurchaseTokenAmountUsd;
+        fastPriceFeed = _fastPriceFeed;
+
+        emit Initialize(
+            _router,
+            _vault,
+            _weth,
+            _usdf,
+            _minExecutionFee,
+            _minPurchaseTokenAmountUsd,
+            _fastPriceFeed
+        );
+    }
+
+    receive() external payable {
+        require(msg.sender == weth, "OrderBook: invalid sender");
+    }
+
+    function setMinExecutionFee(uint256 _minExecutionFee) external onlyGov {
+        minExecutionFee = _minExecutionFee;
+
+        emit UpdateMinExecutionFee(_minExecutionFee);
+    }
+
+    function setMinPurchaseTokenAmountUsd(
+        uint256 _minPurchaseTokenAmountUsd
+    ) external onlyGov {
+        minPurchaseTokenAmountUsd = _minPurchaseTokenAmountUsd;
+
+        emit UpdateMinPurchaseTokenAmountUsd(_minPurchaseTokenAmountUsd);
+    }
+
+    function setGov(address _gov) external onlyGov {
+        gov = _gov;
+
+        emit UpdateGov(_gov);
+    }
+
+    function setFastPriceFeed(address _fastPriceFeed) external onlyGov {
+        fastPriceFeed = _fastPriceFeed;
+
+        emit UpdateFastPriceFeed(_fastPriceFeed);
+    }
+
+    function validatePositionOrderPrice(
+        bool _triggerAboveThreshold,
+        uint256 _triggerPrice,
+        address _indexToken,
+        bool _maximizePrice,
+        bool _raise
+    ) public view returns (uint256, bool) {
+        uint256 currentPrice = _maximizePrice
+            ? IVault(vault).getMaxPrice(_indexToken)
+            : IVault(vault).getMinPrice(_indexToken);
+        bool isPriceValid = _triggerAboveThreshold
+            ? currentPrice > _triggerPrice
+            : currentPrice < _triggerPrice;
+        if (_raise) {
+            require(isPriceValid, "OrderBook: invalid price for execution");
+        }
+        return (currentPrice, isPriceValid);
+    }
+
+    function getTrailingStopOrder(
+        address _account,
+        uint256 _orderIndex
+    )
+        public
+        view
+        override
+        returns (
+            address collateralToken,
+            uint256 collateralDelta,
+            address indexToken,
+            uint256 sizeDelta,
+            bool isLong,
+            uint256 triggerPrice,
+            bool triggerAboveThreshold,
+            uint256 executionFee,
+            uint256 trailingBPS
+        )
