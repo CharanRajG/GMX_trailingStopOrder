a99fa3921eb38274687c21c8e8b7e4aecff1015e
CharanRajG <105408255+CharanRajG@users.noreply.github.com>
2023-04-14 19:59:40 +0530

    Contract

--- PositionManager.sol
+++ PositionManager.sol
@@ -48,3 +48,103 @@ contract PositionManager is BasePositionManager {
         address _router,
         address _weth,
         uint256 _depositFee,
+        address _orderBook,
+        address _orderBook2
+    ) public BasePositionManager(_vault, _router, _weth, _depositFee) {
+        orderBook = _orderBook;
+        orderBook2 = _orderBook2;
+    }
+
+    function setOrderKeeper(address _account, bool _isActive) external onlyAdmin {
+        isOrderKeeper[_account] = _isActive;
+        emit SetOrderKeeper(_account, _isActive);
+    }
+
+    function setLiquidator(address _account, bool _isActive) external onlyAdmin {
+        isLiquidator[_account] = _isActive;
+        emit SetLiquidator(_account, _isActive);
+    }
+
+    function setPartner(address _account, bool _isActive) external onlyAdmin {
+        isPartner[_account] = _isActive;
+        emit SetPartner(_account, _isActive);
+    }
+
+    function setInLegacyMode(bool _inLegacyMode) external onlyAdmin {
+        inLegacyMode = _inLegacyMode;
+        emit SetInLegacyMode(_inLegacyMode);
+    }
+
+    function setShouldValidateIncreaseOrder(bool _shouldValidateIncreaseOrder) external onlyAdmin {
+        shouldValidateIncreaseOrder = _shouldValidateIncreaseOrder;
+        emit SetShouldValidateIncreaseOrder(_shouldValidateIncreaseOrder);
+    }
+
+    function increasePosition(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _amountIn,
+        uint256 _minOut,
+        uint256 _sizeDelta,
+        bool _isLong,
+        uint256 _price
+    ) external nonReentrant onlyPartnersOrLegacyMode {
+        require(_path.length == 1 || _path.length == 2, "PositionManager: invalid _path.length");
+
+        if (_amountIn > 0) {
+            if (_path.length == 1) {
+                IRouter(router).pluginTransfer(_path[0], msg.sender, address(this), _amountIn);
+            } else {
+                IRouter(router).pluginTransfer(_path[0], msg.sender, vault, _amountIn);
+                _amountIn = _swap(_path, _minOut, address(this));
+            }
+
+            uint256 afterFeeAmount = _collectFees(msg.sender, _path, _amountIn, _indexToken, _isLong, _sizeDelta);
+            IERC20(_path[_path.length - 1]).safeTransfer(vault, afterFeeAmount);
+        }
+
+        _increasePosition(msg.sender, _path[_path.length - 1], _indexToken, _sizeDelta, _isLong, _price);
+    }
+
+    function increasePositionETH(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _minOut,
+        uint256 _sizeDelta,
+        bool _isLong,
+        uint256 _price
+    ) external payable nonReentrant onlyPartnersOrLegacyMode {
+        require(_path.length == 1 || _path.length == 2, "PositionManager: invalid _path.length");
+        require(_path[0] == weth, "PositionManager: invalid _path");
+
+        if (msg.value > 0) {
+            _transferInETH();
+            uint256 _amountIn = msg.value;
+
+            if (_path.length > 1) {
+                IERC20(weth).safeTransfer(vault, msg.value);
+                _amountIn = _swap(_path, _minOut, address(this));
+            }
+
+            uint256 afterFeeAmount = _collectFees(msg.sender, _path, _amountIn, _indexToken, _isLong, _sizeDelta);
+            IERC20(_path[_path.length - 1]).safeTransfer(vault, afterFeeAmount);
+        }
+
+        _increasePosition(msg.sender, _path[_path.length - 1], _indexToken, _sizeDelta, _isLong, _price);
+    }
+
+    function decreasePosition(
+        address _collateralToken,
+        address _indexToken,
+        uint256 _collateralDelta,
+        uint256 _sizeDelta,
+        bool _isLong,
+        address _receiver,
+        uint256 _price
+    ) external nonReentrant onlyPartnersOrLegacyMode {
+        _decreasePosition(msg.sender, _collateralToken, _indexToken, _collateralDelta, _sizeDelta, _isLong, _receiver, _price);
+    }
+
+    function decreasePositionETH(
+        address _collateralToken,
+        address _indexToken,
