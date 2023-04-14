fd870fe27760b91549b8110dd2766438463eb133
CharanRajG <105408255+CharanRajG@users.noreply.github.com>
2023-04-14 19:57:30 +0530

    Contract

--- PositionManager.sol
+++ PositionManager.sol
@@ -1,3 +1,50 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.18;
+
+import "./interfaces/IRouter.sol";
+import "./interfaces/IVault.sol";
+import "./interfaces/IOrderBook.sol";
+import "./interfaces/IOrderBook2.sol";
+
+import "../peripherals/interfaces/ITimelock.sol";
+import "./BasePositionManager.sol";
+
+contract PositionManager is BasePositionManager {
+
+    address public orderBook;
+    address public orderBook2;
+    bool public inLegacyMode;
+
+    bool public shouldValidateIncreaseOrder = true;
+
+    mapping (address => bool) public isOrderKeeper;
+    mapping (address => bool) public isPartner;
+    mapping (address => bool) public isLiquidator;
+
+    event SetOrderKeeper(address indexed account, bool isActive);
+    event SetLiquidator(address indexed account, bool isActive);
+    event SetPartner(address account, bool isActive);
+    event SetInLegacyMode(bool inLegacyMode);
+    event SetShouldValidateIncreaseOrder(bool shouldValidateIncreaseOrder);
+
+    modifier onlyOrderKeeper() {
+        require(isOrderKeeper[msg.sender], "PositionManager: forbidden");
+        _;
+    }
+
+    modifier onlyLiquidator() {
+        require(isLiquidator[msg.sender], "PositionManager: forbidden");
+        _;
+    }
+
+    modifier onlyPartnersOrLegacyMode() {
+        require(isPartner[msg.sender] || inLegacyMode, "PositionManager: forbidden");
+        _;
+    }
+
+    constructor(
+        address _vault,
+        address _router,
+        address _weth,
+        uint256 _depositFee,
